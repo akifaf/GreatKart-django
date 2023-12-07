aef78538511d2bcc2f0a1a32c0385a718c62a0f1
Fathima Zohara <fathimaakifa35@gmail.com>
2023-12-07 08:47:03 +0530

    store and carts functionality

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
     path('accounts/', include('accounts.urls')),
+    path('carts/', include('carts.urls')),
     path('customadmin/', include('customadmin.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/models.py
+++ store/models.py
@@ -22,3 +22,27 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name    
+
+class VaritationManager(models.Manager):
+    def colors(self):
+        return super(VaritationManager, self).filter(variation_category = 'color', is_active=True)    
+    
+    def sizes(self):
+        return super(VaritationManager, self).filter(variation_category = 'size', is_active=True)    
+
+varitation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size')
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=varitation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    objects = VaritationManager()
+
+    def __str__(self):
+        return self.variation_value
