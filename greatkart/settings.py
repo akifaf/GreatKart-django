fc01d694fbf0120c20a9d2eb465f6266a05cf304
Fathima Zohara <fathimaakifa35@gmail.com>
2024-01-14 08:43:58 +0530

    settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -120,24 +120,24 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'greatkart',
-        'USER': 'postgres',
-        'PASSWORD': 'hellopostakifa',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'greatkart',
+#         'USER': 'postgres',
+#         'PASSWORD': 'hellopostakifa',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
